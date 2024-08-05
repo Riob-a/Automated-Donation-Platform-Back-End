4bf7670d769a429dfb3b8418bda12d41df1476b1
Derrick <riobad74@gmail.com>
2024-08-05 09:43:37 +0300

    added the rest

--- Back-end/models.py
+++ Back-end/models.py
@@ -45,3 +45,52 @@ class Charity(db.Model):
             'website': self.website,
             'approved': self.approved,
             'image_url': self.image_url
+        }
+
+class Donation(db.Model):
+    __tablename__ = 'donations'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    anonymous = db.Column(db.Boolean, default=False)
+    donation_date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    charity_id = db.Column(db.Integer, db.ForeignKey('charities.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Donation {self.amount} by User {self.user_id}>"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'amount': self.amount,
+            'anonymous': self.anonymous,
+            'donation_date': self.donation_date,
+            'user_id': self.user_id,
+            'charity_id': self.charity_id
+        }
+
+class Beneficiary(db.Model):
+    __tablename__ = 'beneficiaries'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    story = db.Column(db.Text, nullable=True)
+    charity_id = db.Column(db.Integer, db.ForeignKey('charities.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Beneficiary {self.name}>"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'story': self.story,
+            'charity_id': self.charity_id
+        }
+
+class TokenBlacklist(db.Model):
+    __tablename__ = 'token_blacklist'
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return f"<TokenBlacklist {self.token}>"
