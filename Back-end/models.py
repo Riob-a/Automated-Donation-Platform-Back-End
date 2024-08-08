52dd67c582ba2bc6245be3565eb409167f6d50db
Derrick <riobad74@gmail.com>
2024-08-08 12:41:29 +0300

    added admin model

--- Back-end/models.py
+++ Back-end/models.py
@@ -119,4 +119,20 @@ class Application(db.Model):
             'status': self.status
         }
 
+class Admin(db.Model):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)  # Store hashed password
+
+    def __repr__(self):
+        return f"<Admin {self.username}>"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+        }
 
