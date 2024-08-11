3ec6754a7b980bc4669b52b18cca13679ae35e0e
Derrick <riobad74@gmail.com>
2024-08-11 12:03:34 +0300

    added port

--- app.py
+++ app.py
@@ -1227,5 +1227,6 @@ def admin_logout():
 #     return decrypted_token['jti'] in BLACKLIST
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
