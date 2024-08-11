f756d1095eecb72b3e9925b5019a8ff32935a200
Derrick <riobad74@gmail.com>
2024-08-11 12:36:15 +0300

    added flassger

--- app.py
+++ app.py
@@ -1227,6 +1227,6 @@ def admin_logout():
 #     return decrypted_token['jti'] in BLACKLIST
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
 
